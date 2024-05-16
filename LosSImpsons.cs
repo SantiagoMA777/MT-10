5d5370ee87f1c0422b8ae9a4d5824582ccdee3e4
SantiagoMA777 <thiago12asprilla@gmail.com>
2024-05-16 15:46:12 -0500

    Add files via upload

---
+++ LosSImpsons.cs
@@ -0,0 +1,27 @@
+/******************************************************************************
+Autor: Juan Arango y andres martinez
+Fecha creacion: 2024-05.16
+Descripcion:Los Simpson están organizando un torneo
+de videojuegos y necesitan un programa en C# para
+mostrar los nombres de los participantes. Completa
+el espacio en blanco para imprimir los nombres en la consola:
+ 
+
+*******************************************************************************/
+using System;
+class HelloWorld
+{
+  static void Main()
+  {
+      string participante1 = "Bart";
+      string participante2 = "Jimbo";
+      string participante3 = "Milhouse";
+      string participante4 = "Apu";
+      Console.WriteLine("Integrantes del torrneo: ");
+      Console.WriteLine(participante1);
+      Console.WriteLine(participante2);
+      Console.WriteLine(participante3);
+      Console.WriteLine(participante4);
+  }
+  
+}
