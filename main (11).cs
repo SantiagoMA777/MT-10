9cf1256dbcc43c7ba65aa438a34fb8c4bf2c2c0e
SantiagoMA777 <thiago12asprilla@gmail.com>
2024-07-10 16:31:32 -0500

    Add files via upload

---
+++ main (11).cs
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+using System;
+class HelloWorld {
+  static void Main() {
+    string frases
+    
+    "si un dia sientes que no sientes intenta sentir el por que no sientes";
+    "sacrificarse por tener un buen futuro no es tiempo perdido es tiempo bien aplicado";
+    "si dices que no puedes no podras, pero si ves que no puedes y lo intentas hasta que puedas podras";
+    
+    random aleatorio = new Random();
+    int posicion = aleatorio.Next(0,2)
+    
+    Console.WriteLine(frases[posicion]);
+      }
+}
